96b7062559064ed4727ed661a8ae3aa2ef595f99
thuyngavo <lisa.vo49@gmail.com>
2019-06-17 14:33:38 -0500

    adding code for navbar to public/components and importing to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Navbar from ".public/components/Navbar";
 
 function App() {
   return (
