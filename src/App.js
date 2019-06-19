a21a755ae24f45ddf5a849ffc25ed169ebd9294c
thuyngavo <lisa.vo49@gmail.com>
2019-06-18 21:02:04 -0500

    adding code for footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Navbar from ".public/components/Navbar";
+import Navbar from "./components/Navbar";
+import Footer from ".components/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function footer() {
+    return (
+        <footer>
+            <span>
+                Final Project 2019
+            </span>
+        </footer>
+    );
+}
+
+export default footer;
