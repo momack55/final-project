a57c1ee34e8268853992c7134849092abd00242a
thuyngavo <lisa.vo49@gmail.com>
2019-06-19 14:02:58 -0500

    code for page container, wrapper, eventCard and card buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+//import react
 import React from 'react';
+
+//import router
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 //import logo from './logo.svg';
+
+//import styling 
 import './App.css';
 
-//import react components
+//import components
 import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
 import Wrapper from "./components/wrapper"
 
 //import pages
-import Home from ".pages/home";
-import Profile from ".pages/profile"
-import Search from ".pages/search"
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import Search from "./pages/search";
+// import Signup from "./pages/signup";
+// import Login from "./pages/login";
 
 
 function App() {
@@ -23,6 +31,8 @@ function App() {
           <Route exact path="/" component = {Home} />
           <Route exact path="/profile" component = {Profile} />
           <Route exact path="/search" component = {Search} />
+          {/* <Route exact path="/signup" component = {Signup} />
+          <Route exact path="/login" component = {Login} /> */}
         </Wrapper>
         <Footer />
       </div>
