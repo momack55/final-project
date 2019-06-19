763f04d49ed5744a8c1b6711048e30add2598970
thuyngavo <lisa.vo49@gmail.com>
2019-06-18 21:16:11 -0500

    set up additional component files for event cards to search and buttons needed / imported page files to app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //import logo from './logo.svg';
 import './App.css';
+
+//import react components
 import Navbar from "./components/Navbar";
 import Footer from ".components/footer";
+
+//import pages
+import Home from ".pages/home";
+import Profile from ".pages/profile"
+import Search from ".pages/search"
+
 
 function App() {
   return (
