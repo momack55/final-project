23acc3eee7748ed5801d0474b572d68cb5e2aa6e
thuyngavo <lisa.vo49@gmail.com>
2019-06-25 20:19:56 -0500

    first round of front end js for Discover page

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -10,13 +10,17 @@ import Card from "../components/Card";
 class Discover extends Component {
   state = {
     image: "",
-    match: false,
-    matchCount: 0
+    title: "",
+    description: "",
+    url: "", 
+    date: "",
+    save: false,
+    saveCount: 0
   };
 
-  // When the component mounts, load the next dog to be displayed
+  // When the component mounts, load the next event to be displayed
   componentDidMount() {
-    this.loadNextDog();
+    this.loadNextEvent();
   }
 
   handleBtnClick = event => {
@@ -27,24 +31,25 @@ class Discover extends Component {
     const newState = { ...this.state };
 
     if (btnType === "pick") {
-      // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-      newState.match = 1 === Math.floor(Math.random() * 5) + 1;
+      // Set newState.save to true when buton is picked 
+      newState.save = 1;
+      //newState.save = true;
 
-      // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-      newState.matchCount = newState.match
-        ? newState.matchCount + 1
-        : newState.matchCount;
+      // Set newState.saveCount equal to its current value or its current value + 1 when user saves event
+      newState.saveCount = newState.save
+        ? newState.saveCount + 1
+        : newState.saveCount;
     } else {
-      // If we thumbs down'ed the dog, we haven't matched with it
-      newState.match = false;
+      // If we thumbs down'ed the event, we haven't saveed with it
+      newState.save = false;
     }
-    // Replace our component's state with newState, load the next dog image
+    // Replace our component's state with newState, load the next event image
     this.setState(newState);
-    this.loadNextDog();
+    this.loadNextEvent();
   };
 
   loadNextDog = () => {
-    API.getRandomDog()
+    API.getEvent()
       .then(res =>
         this.setState({
           image: res.data.message
@@ -65,10 +70,10 @@ class Discover extends Component {
         <Col size="md-12">
         <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
         <h1 className="text-center">
-          You have saved {this.state.matchCount} events so far!
+          You have saved {this.state.saveCount} events so far!
         </h1>
-        {/* <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">
-          Yay! You have event matches!!! Go to your profile to check them out. 
+        {/* <Alert style={{ opacity: this.state.save ? 1 : 0 }} type="success">
+          Yay! You have event savees!!! Go to your profile to check them out. 
         </Alert> */}
         </Col>
         </Row>
