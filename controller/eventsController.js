84da2d6d1a494788dd70cb8caa774f0b6064152f
thuyngavo <lisa.vo49@gmail.com>
2019-06-27 20:39:02 -0500

    rendering dangerous html to page bwahahaha

--- controller/eventsController.js
+++ controller/eventsController.js
@@ -1,34 +1,37 @@
 const db = require("../models");
 
 module.exports = {
+  // get: read all events in db
   findAll: function(req, res) {
     db.Event
       .find(req.query)
-      .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //get by id: get one event by id   
   findById: function(req, res) {
     db.Event
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //create: save event to db    
   create: function(req, res) {
     db.Event
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //post: update event in db
   update: function(req, res) {
     db.Event
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //delete: delete event by id
   remove: function(req, res) {
-    db.Event
-      .findById({ _id: req.params.id })
+    db.Event.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -4,33 +4,24 @@ import Container from "../components/container";
 import Row from "../components/row";
 import Col from "../components/Col";
 import API from "../utils/API";
-import Card from "../components/Card";
-import Alert from "../components/Alert";
+//import Card from "../components/Card";
+//import Alert from "../components/Alert";
 
 class Discover extends Component {
   state = {
-<<<<<<< HEAD
-    image: "",
-    title: "",
-    description: "",
-    url: "",
-    date: "",
-    save: false,
-    saveCount: 0
-=======
     event: [],
+    description: [],
+    startTime: [],
+    image: [],
+    url: [],
     match: false,
     matchCount: 0
->>>>>>> f219c19b4c26ff1d134834e87eade2c67952dc32
   };
 
   // When the component mounts, load the next event to be displayed
   componentDidMount() {
     this.loadNextEvent();
-<<<<<<< HEAD
-=======
-    console.log()
->>>>>>> f219c19b4c26ff1d134834e87eade2c67952dc32
+    console.log();
   }
 
   // handleBtnClick = event => {
@@ -40,36 +31,6 @@ class Discover extends Component {
   //   // We'll modify this object and use it to set our component's state
   //   const newState = { ...this.state };
 
-<<<<<<< HEAD
-    if (btnType === "pick") {
-      // Set newState.save to true when buton is picked 
-      newState.save = 1;
-      //newState.save = true;
-
-      // Set newState.saveCount equal to its current value or its current value + 1 when user saves event
-      newState.saveCount = newState.save
-        ? newState.saveCount + 1
-        : newState.saveCount;
-    } else {
-      // If we thumbs down'ed the event, we haven't saveed with it
-      newState.save = false;
-    }
-    // Replace our component's state with newState, load the next event image
-    this.setState(newState);
-    this.loadNextEvent();
-  };
-
-  //function to get the next event 
-  loadNextDog = () => {
-    API.getEvent()
-      .then(res =>
-        this.setState({
-          image: res.data.message,
-          title: res.data.message,
-          description: res.data.message,
-          url: res.data.message,
-          date: res.data.message
-=======
   //   if (btnType === "pick") {
   //     // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
   //     newState.match = 1 === Math.floor(Math.random() * 5) + 1;
@@ -87,21 +48,32 @@ class Discover extends Component {
   //   this.loadNextEvent();
   // };
 
+  // createMarkup = () => {
+  //   return 
+  // }
+
+
+
   loadNextEvent = () => {
     API.getEvents()
       .then(res =>
         this.setState({
-          event: res.data.events.event[0].title
->>>>>>> f219c19b4c26ff1d134834e87eade2c67952dc32
+          event: res.data.events.event[0].title,
+          description: res.data.events.event[0].description,
+          startTime: res.data.events.event[0].start_time,
+          //image: res.data.events.event[0].image.medium.url,
+          //url: res.data.events.event[0].venue.url,
         })
+        
       )
       .catch(err => console.log(err));
   };
+  
+  renderDescription = () => ({__html: this.state.description})
 
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <Hero backgroundImage="https://thumbs.gfycat.com/RightDisloyalGallinule-size_restricted.gif" >
           <h1>Local Events</h1>
           <h3 className="text-center">Thumbs up on any events you want to save for later</h3>
@@ -109,29 +81,15 @@ class Discover extends Component {
         <Container style={{ marginTop: 30 }}>
           <Row>
             <Col size="md-12">
-              <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
-              <h1 className="text-center">
-                You have saved {this.state.saveCount} events so far!
-        </h1>
-              <Alert style={{ opacity: this.state.save ? 1 : 0 }} type="success">
-                Yay! You have events saved!!! Go to your profile to check them out.
-        </Alert>
+              {/* <Card title={this.state.event} description={this.state.description}/> */}
+              <h1>Title: {this.state.event}</h1>
+              <h4 dangerouslySetInnerHTML={this.renderDescription()}></h4>
+              <h4>Start Time: {this.state.start_time}</h4>
+              {/* <h4>Image: {this.state.image.medium.url}</h4> */}
+              {/* <h4>Link: {this.state.venue.url}</h4> */}
             </Col>
           </Row>
         </Container>
-=======
-      <Hero backgroundImage="https://thumbs.gfycat.com/RightDisloyalGallinule-size_restricted.gif" >
-        <h1>Local Events</h1>
-        <h3 className="text-center">Thumbs up on any events you want to save for later</h3>
-      </Hero>
-      <Container style={{ marginTop: 30 }}>
-      <Row>
-        <Col size="md-12">
-        <h1>Title: {this.state.event}</h1>
-        </Col>
-        </Row>
-      </Container>
->>>>>>> f219c19b4c26ff1d134834e87eade2c67952dc32
       </div>
 
     );
