7583717c3fd7ce05023ecf97b27c83cc3be397f7
momack55 <memccormack55@gmail.com>
2019-07-04 20:18:37 -0500

    Deploying to heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const routes = require("./routes");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -30,16 +31,23 @@ mongoose
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
 
-// Passport middleware
-
-
-
 //Passport Config
 require("./config/passport")(passport);
 
 //Routes
 app.use("/api/users", users);
 app.use(routes);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+
+}
 
 
 
