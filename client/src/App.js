4aea53ac18eb8a4100ab4932c04e6c449d3fa68c
momack55 <memccormack55@gmail.com>
2019-07-04 19:14:34 -0500

    passport fully functioning with database

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -18,6 +21,27 @@ import About from "./pages/About";
 import Discover from "./pages/Discover";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
@@ -31,7 +55,10 @@ function App() {
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/discover" component={Discover} />
           <Route exact path="/auth/register" component={Register} />
-          <Route exact path="/auth/login" component={Login} />        
+          <Route exact path="/auth/login" component={Login} />  
+          <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>      
           </Wrapper>
         <Footer />
       </div>
