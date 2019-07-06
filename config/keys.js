59e542fcebc7bffc8fb48fe4b6abaa417fb08689
momack55 <memccormack55@gmail.com>
2019-07-06 11:58:54 -0500

    corrected DB error with events and connected to Heroku DB

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,13 @@
 
+// module.exports = {
+//     mongoURI: "mongodb+srv://memccormack:Tishie55@fun-finder-nc4k5.mongodb.net/test?retryWrites=true&w=majority",
+//     secretOrKey: "secret"
+
+//   };
+
 module.exports = {
-    mongoURI: "mongodb+srv://memccormack:Tishie55@fun-finder-nc4k5.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
+  mongoURI: "mongodb://FunFinder2:FunFinder2@ds347367.mlab.com:47367/heroku_j28v20p6",
+  secretOrKey: "secret"
 
-  };
-
+};
 
